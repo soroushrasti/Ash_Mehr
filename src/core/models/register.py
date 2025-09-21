9641a1902bb424447e8bd5fa92db198a93f7d9be
heidarian <heidarian66@gmail.com>
2025-09-21 17:42:03 +0330

    edit signup apis

--- src/core/models/register.py
+++ src/core/models/register.py
@@ -93,13 +93,14 @@ class Register(Base):
         db_session.add(self)
         db_session.commit()
         db_session.refresh(self)
+
         children_data = self.__dict__.pop("children_of_reg", None)
         if children_data:
             for child in children_data:
-                child_obj = ChildrenOfRegister(**child.dict(), RegisterID = self.RegisterID)
+                child_obj = ChildrenOfRegister(**child, RegisterID = self.RegisterID)
             db_session.add(child_obj)
             db_session.commit()
-            db_session.refresh(child_obj)
+            db_session.refresh()
         return self
 
     def edit_register(self, db_session, user_data):
